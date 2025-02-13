334c1979bba9b05564c18dd16ac15d376b139d75
Ahmed Skhiri <ahmed.skhiri@insat.ucar.tn>
2025-02-13 22:28:42 +0100

    minigame.js

--- minigame.js
+++ minigame.js
@@ -6,61 +6,51 @@ function playagain(){
 }
 document.getElementById('attempts').textContent=attempts;
 function guessgame(x){
-    if(attempts>=3){
-        console.log("you have lost the game :( !! please try again");
-        let again=confirm("do you want to play again ?");
-        {
-            if(again==true){
-                playagain();
-
-            }
-            else{
-                console.log("thank you for playing");
+    while(attempts < 3){
+        let x=Math.random();
+        let guess = prompt("please enter your guess");
+        while(isNaN(guess)){
+            guess = prompt("please enter a valid number");
+        }
+        guess = Number(guess);
+        if(x == "facile"){
+            if(guess == Math.floor(x* 10)){
+                console.log("you have guessed right congrats ");
+                document.getElementById('attempts').textContent = 0;
+                return;
             }
         }
-    }
-    else{
-    if(x=="facile"){
-        console.log("facile");
-        let guess=prompt("please enter your guess");
-        if(guess==Math.floor(Math.random()*10))
-            {console.log("you have guessed right congrats ");
-                document.getElementById('attempts').textContent=0;
-                attempts=0;
+        if(x == "int"){
+            if(guess == Math.floor(x* 100)){
+                console.log("you have guessed right congrats ");
+                document.getElementById('attempts').textContent = 0;
+                return;
             }
-        else{
-            console.log("please try again :< ");
-            attempts++;
-            document.getElementById('attempts').textContent=attempts;
         }
-    }
-    if(x=="int"){
-        console.log("intermediare");
-        let guess=prompt("please enter your guess");
-        if(guess==Math.floor(Math.random()*100))
-            {console.log("you have guessed right congrats ");
-                document.getElementById('attempts').textContent=0;
-                attempts=0;
+        if(x == "diff"){
+            if(guess == Math.floor(x*1000)){
+                console.log("you have guessed right congrats ");
+                document.getElementById('attempts').textContent = 0;
+                return;
             }
-        else{
-            console.log("please try again :< ");
-            attempts++;
-            document.getElementById('attempts').textContent=attempts;
         }
+        console.log("please try again :< ");
+        attempts++;
+        document.getElementById('attempts').textContent = attempts;
     }
-    if(x=="diff"){
-        console.log("difficile ");
-        let guess=prompt("please enter your guess");
-        if(guess==Math.floor(Math.random()*10))
-            {console.log("you have guessed right congrats ");
-                document.getElementById('attempts').textContent=0;
-                attempts=0;
+    
+    document.getElementById('attempts').textContent=attempts;  
+        if(attempts>=3){
+            console.log("you have lost the game :( !! please try again");
+            let again=confirm("do you want to play again ?");
+            {
+                if(again==true){
+                    playagain();
+    
+                }
+                else{
+                    console.log("thank you for playing");
+                }
             }
-        else{
-            console.log("please try again :< ");
-            attempts++;
-            document.getElementById('attempts').textContent=attempts;
-        }}
-        document.getElementById('attempts').textContent=attempts;  
-
-}}
+        }
+}
